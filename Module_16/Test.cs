05a6b5642fff09e32b6206340486b9c78d0f6a67
ivan <gervadin.nomer1@gmail.com>
2023-03-23 13:11:55 +0300

    Добавьте файлы проекта.

---
+++ Module_16/Test.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Practices.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AddMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Additional(10, 1) == 11);
+        }
+
+        [Test]
+        public void SubtractionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Subtraction(10, 1) == 9);
+        }
+
+        [Test]
+        public void MiltiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(10, 2) == 20);
+        }
+
+        [Test]
+        public void DivisionMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(10, 2) == 5);
+        }
+    }
+}
